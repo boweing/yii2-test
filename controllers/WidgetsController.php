feb2d0c25370be16caa4e1daeafdd6692ee9eaec
boweing <boweing@gmail.com>
2015-03-11 15:30:49 +0700

    update
    update

---
+++ controllers/WidgetsController.php
@@ -0,0 +1,27 @@
+<?php
+namespace app\controllers;
+use yii\web\Controller;
+use yii\data\ActiveDataProvider;
+use app\models\Contact;
+class WidgetsController extends Controller{
+    
+    public function actionGrid(){
+        $dataProvider = new ActiveDataProvider([
+            'query'=>Contact::find(),
+        ]);
+        return $this->render('grid',['dataProvider'=>$dataProvider]);
+    }
+    
+    public function actionList(){
+        $dataProvider = new ActiveDataProvider([
+           'query'=>Contact::find(), 
+        ]);
+        return $this->render('list',['dataProvider'=>$dataProvider]);   
+    }
+    
+    public function actionDetail($id){
+        $model = Contact::findOne($id);
+           
+        return $this->render('Detail',['model'=>$model]);   
+    }
+}
